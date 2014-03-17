e5acdc0a70829457f0b004ed1b59863740526a92
Ondrej Pokorny <ondrej.pokorny@honeywell.com>
2014-03-17 17:05:19 +0100

    opassign zaklad

--- source/ev4d/scenegraph/hierarchygraph.d
+++ source/ev4d/scenegraph/hierarchygraph.d
@@ -12,6 +12,18 @@ struct HierarchyGraph(T)
 	HierarchyGraph* parent = null;
 
 	HierarchyGraph[] children;
+
+	// ma binarni operator pro tohle smysl? nejspis ne, strukturu to nemeni, potreba udelat opassign
+	// kdyz nekdo zavola binarni operator pro tohle, tak by se to "melo chovat korektne" ve smyslu, ze ty objekty jsou validni uz predem (spoji dve casti (ruzneho) stromu)
+	// mozna to spis zakazat tuhle moznost... rozmyslet, melo by smysl pouze pokud by byly objekty jen vlistech nebo i v uzlech jak je to ted? 
+	HierarchyGraph[] opBinary(string op)(S rhs)
+	if (is(typeof(leaf == rhs) : bool))
+	{}
+	HierarchyGraph[] opBinary(string op)(HierarchyGraph rhs){}
+	HierarchyGraph[] opBinary(string op)(HierarchyGraph[] rhs){}
+
+	opAssign
+	udelat jak ~= tak i = ... kopiruje obsah, ale musi prenastavit parenta
 /*
 	this() @safe pure nothrow
 	{}
@@ -20,6 +32,26 @@ struct HierarchyGraph(T)
 	{}*/
 
 
+}
+
+unittest
+{
+	HierarchyGraph!float a;
+	assert((a ~ 1.0) ==);
+	assert((a ~ 1.0f) ==);
+	assert((a ~ 1));
+	assert((a ~ 'c');
+
+	a ~= 1.0;
+	a ~= 1.0f;
+	a ~= 1;
+	a ~= 'c';
+
+	HierarchyGraph!float b;
+	assert((a ~ b) == );
+	HierarchyGraph!int c;
+	HierarchyGraph!char d; // this should not be working
+	etc...
 }
 
 alias HierarchyGraphIterator HGraphIterator;
