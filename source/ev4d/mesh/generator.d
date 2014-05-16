eb92de92aa8d9dfe99dbdc0ba500da779a8d8980
Ondrej Pokorny <pokorny.ondrej@gmail.com>
2014-05-16 22:06:29 +0200

    removing test code from renderer, added for lookup to generator

--- source/ev4d/mesh/generator.d
+++ source/ev4d/mesh/generator.d
@@ -49,6 +49,39 @@ public:
 unittest
 {
 	// TODO
+	/*
+	CubeVertexesEmitor!(float) emitor;
+
+	vec3 arrv[8];
+	static float rotangle = 0;
+	rotangle+=1;
+	glRotatef(rotangle, 1, 1, 1);
+	glBegin(GL_POINTS);
+		int i = 0;
+		foreach(vec3 v; emitor)
+		{
+			glVertex3fv(v.value_ptr);
+			arrv[i] = v;
+			i++;
+			//writeln(v);
+		}
+	glEnd();
+	//writeln();
+
+	CubeTrisEmitor!(int) trisemit;
+
+	glBegin(GL_TRIANGLES);
+		foreach (Vector!(int, 3) id; trisemit)
+		{
+			glColor3b(cast(byte)(id.x*10), cast(byte)(id.y*10), cast(byte)(id.z*30));
+
+			glVertex3fv(arrv[id.x].value_ptr);
+			glVertex3fv(arrv[id.y].value_ptr);
+			glVertex3fv(arrv[id.z].value_ptr);
+		}
+
+	glEnd();
+	*/
 }
 
 struct CubeTrisEmitor(T)
--- source/ev4d/rendersystem/renderer.d
+++ source/ev4d/rendersystem/renderer.d
@@ -19,6 +19,7 @@ public:
 	void render()
 	{
 		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		foreach(GeneralTechnique t; techniques)
@@ -48,38 +49,6 @@ public:
 			}
 
 			t.render();
-///////////////////////////////////////////////////////////////////////////////////////////////
-			CubeVertexesEmitor!(float) emitor;
-
-			vec3 arrv[8];
-			static float rotangle = 0;
-			rotangle+=1;
-			glRotatef(45, 1, 1, 1);
-			glBegin(GL_POINTS);
-				int i = 0;
-				foreach(vec3 v; emitor)
-				{
-					glVertex3fv(v.value_ptr);
-					arrv[i] = v;
-					i++;
-					//writeln(v);
-				}
-			glEnd();
-			//writeln();
-
-			CubeTrisEmitor!(int) trisemit;
-
-			glBegin(GL_TRIANGLES);
-				foreach (Vector!(int, 3) id; trisemit)
-				{
-					glColor3b(cast(byte)(id.x*20), cast(byte)(id.y*20), cast(byte)(id.z*30));
-
-					glVertex3fv(arrv[id.x].value_ptr);
-					glVertex3fv(arrv[id.y].value_ptr);
-					glVertex3fv(arrv[id.z].value_ptr);
-				}
-
-			glEnd();
 		}
 	}
 }
