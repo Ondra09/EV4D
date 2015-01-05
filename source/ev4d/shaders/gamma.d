b1ef3d7f73ea8c1af6d6018a3527fcf9ddf510ea
Ondrej Pokorny <pokorny.ondrej@gmail.com>
2015-01-05 23:14:32 +0100

    fog pixels shader function
    * not tested
    * not integraed

--- source/ev4d/shaders/gamma.d
+++ source/ev4d/shaders/gamma.d
@@ -1,7 +1,7 @@
 module ev4d.shaders.gamma;
 
 
-const string pixelShaderFunctions = 
+immutable string pixelShaderFunctions = 
 "
 float gamma = 1/2.2; // in [2, 2.4]
 
@@ -12,4 +12,27 @@ float gammaCorrection(float light)
 
 //vec3 color = lightingModel( … );
 //FragColor = vec4( pow( color, vec3(1.0/Gamma) ), 1.0 );
+";
+
+immutable string fogPixelShaderFunction = 
+"
+float linearFog(float dst, float maxDst, float minDst)
+{
+	return (maxDst - dst) / (maxDst - minDst);
+}
+
+float expFog(float dst, float density)
+{
+	return exp(-pow(density*dist, 2.0)); // pow not necessary
+}
+
+vec3 computeFog(float dst, float density, vec4 shadeColor)
+	
+	fogFactor = expFog(dst, density);
+
+	fogFactor = clamp( fogFactor, 0.0, 1.0 );
+
+	vec3 color = mix( fog.color, shadeColor.xyz, fogFactor );
+
+	return color;
 ";
